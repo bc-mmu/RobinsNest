1c4aebdc9fd1c3653d5c1111a22131b97b4a2f80
bc-mmu <11059293@stu.mmu.ac.uk>
2014-03-03 21:12:04 +0000

    Added links to merchandise app from main page

--- robinsnest/header.php
+++ robinsnest/header.php
@@ -25,12 +25,14 @@ if ($loggedin)
          "<li><a href='friends.php'>Friends</a></li>" .
          "<li><a href='messages.php'>Messages</a></li>" .
          "<li><a href='profile.php'>Edit Profile</a></li>" .
+		 "<li><a href='shop-app.html'>Merchandise</a></li>" .
          "<li><a href='logout.php'>Log out</a></li></ul><br />";
 }
 else
 {
     echo ("<br /><ul class='menu'>" .
          "<li><a href='index.php'>Home</a></li>" .
+		 "<li><a href='shop-app.html'>Merchandise</a></li>" .
          "<li><a href='signup.php'>Sign up</a></li>" .
          "<li><a href='login.php'>Log in</a></li></ul><br />" .
          "<span class='info'>&#8658; You must be logged in to " .
